8b983155fb38629e33e3a5f6a82dfcbd07c5a1bc
Kiran <kiran@gmail.com>
2025-05-21 05:57:35 +0500

    updated

--- Applicant/Applicant.java
+++ Applicant/Applicant.java
@@ -1,5 +1,35 @@
 package Applicant;
 
-public class Applicant {
+import Authentication.Gender;
+import Authentication.Users;
 
-}
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Applicant extends Users {
+
+
+        private ArrayList<ApplicationForm> submittedApplications;
+
+        public Applicant(String firstName, String lastName, String password, String securityAnswer,
+                         String cnic, LocalDate dateOfBirth, Gender gender, String phone, String email) {
+            super(firstName, lastName, password, securityAnswer, cnic, dateOfBirth, gender, phone, email);
+
+            this.submittedApplications = new ArrayList<>();
+        }
+
+        // Add a submitted application
+        public void addApplication(ApplicationForm application) {
+            submittedApplications.add(application);
+        }
+
+        // Get all submitted applications
+        public ArrayList<ApplicationForm> getSubmittedApplications() {
+            return submittedApplications;
+        }
+
+    }
+
+
+
+
